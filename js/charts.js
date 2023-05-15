b91a8d5448389f527fae916329d173d914705c1d
Benjtalkshow <chinedubenj@gmail.com>
2023-05-14 17:32:06 -0700

    I changed and added so many things like, using material icon, removing and inserting new classes in the chart boxes, added and removing background colors, chart box left borders and so on

--- js/charts.js
+++ js/charts.js
@@ -56,10 +56,11 @@ new Chart(box_larger, {
             label: '# of Votes',
             data: [12, 19, 3, 5, 2, 3],
             borderWidth: 1,
-            backgroundColor: ['skyblue', 'Orange', 'Green', 'Tomatoe', 'Red', 'Blue']
+            backgroundColor: ['skyblue', 'Orange', 'Green', 'Tomatoe', 'Red', 'Blue'],
+            fill: 'red' 
         }]
     },
-    options: {
+    options:  {
         layout: {
 width: '5px'
         },
@@ -70,3 +71,26 @@ width: '5px'
         }
     }
 });
+
+
+google.charts.load('current', {'packages':['corechart']});
+      google.charts.setOnLoadCallback(drawChart);
+
+      function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+          ['Year', 'Sales', 'Expenses'],
+          ['2013',  1000,      400],
+          ['2014',  1170,      460],
+          ['2015',  660,       1120],
+          ['2016',  1030,      540]
+        ]);
+
+        var options = {
+          title: 'Company Performance',
+          hAxis: {title: 'Year',  titleTextStyle: {color: '#333'}},
+          vAxis: {minValue: 0}
+        };
+
+        var chart = new google.visualization.AreaChart(document.getElementById('chart_div'));
+        chart.draw(data, options);
+      }
